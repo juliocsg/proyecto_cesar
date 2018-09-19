759ec38164bf1a406cf79f172a6cd067f63f131c
OYM-DEV7 <oymdev7@oym.com.py>
2018-09-19 10:35:27 -0400

    base node

--- Cursos web/NodeJs/03- Bases node/app.js
+++ Cursos web/NodeJs/03- Bases node/app.js
@@ -1,12 +1,39 @@
-const { crearArchivo } = require('./multiplicar/multiplicar');
-
-
-let base = '5';
-
-console.log(process.argv);
-
-crearArchivo(base)
-    .then((archivo) => {
-        console.log(`Archivo creado: ${archivo}`)
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
     })
-    .catch(e => console.log(e));
+    .help()
+    .argv;
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then((archivo) => {
+                console.log(`Archivo creado: ${archivo}`)
+            })
+            .catch(e => console.log(e));
+        break;
+    default:
+        console.log('Comando no reconocido');
+
+}
+
+//console.log(argv.base);
+
+//let parametro = argv[2];
+//let base = parametro.split('=')[1];
+/*console.log(base);*/
--- Cursos web/NodeJs/03- Bases node/multiplicar/multiplicar.js
+++ Cursos web/NodeJs/03- Bases node/multiplicar/multiplicar.js
@@ -1,32 +1,46 @@
 const fs = require('fs');
 
-crearArchivo = (base) => {
+let listarTabla = (base, limite = 10) => {
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${ base } * ${ i } = ${ base * i }`);
+    }
+}
+
+crearArchivo = (base, limite = 10) => {
 
     return new Promise((resolve, reject) => {
         let dato = '';
         if (!Number(base)) {
-            reject(`El valor introducido ${ base } no es un número`);
+            reject(`
+                    El valor introducido ${ base }
+                    no es un número `);
             return;
         }
         //let tabla = 0;
-        for (let i = 1; i <= 10; i++) {
+        for (let i = 1; i <= limite; i++) {
             //let tabla = (i + 1) * 2;
-            dato += `${ base } * ${ i } = ${ base * i}\n`;
+            dato += `
+                    ${ base } * ${ i } = ${ base * i }\
+                    n `;
         }
 
         const data = new Uint8Array(Buffer.from(dato));
-        fs.writeFile(`Tablas/tabla-${ base }.txt`, data, (err) => {
+        fs.writeFile(`
+                    Tablas / tabla - ${ base }-al-${ limite }.txt `, data, (err) => {
 
             if (err)
                 reject(err)
             else
-                resolve(`tabla-${ base }.txt`);
+                resolve(`
+                    tabla - ${ base }.txt `);
 
-            console.log(`El archivo tabla-${ base }.ext ha sido creado`);
+            console.log(`
+                    El archivo tabla - ${ base }.ext ha sido creado `);
         });
     });
 }
 
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    listarTabla
 }
