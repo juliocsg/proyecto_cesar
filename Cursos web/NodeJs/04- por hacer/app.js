b91f938466b283220d0efb181417d1b25dbe0abf
OYM-DEV7 <oymdev7@oym.com.py>
2018-10-01 11:41:07 -0400

    por hacer terminaciòn node

--- Cursos web/NodeJs/04- por hacer/app.js
+++ Cursos web/NodeJs/04- por hacer/app.js
@@ -1,21 +1,31 @@
-const argv = require('yargs').argv;
-
-console.log(argv);
-
+const argv = require('./config/yargs').argv;
+const porHacer = require('./por-hacer/por-hacer');
+const colors = require('colors');
 //devuelve lo que va a escribir dentro del comando
 let comando = argv._[0];
 
 switch (comando) {
     case 'crear':
-        console.log('Crear por hacer');
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
         break;
     case 'listar':
-        console.log('Listar todas las tareas por hacer');
+        let listado = porHacer.getListado();
+        for (let tarea of listado) {
+            console.log('========Por Hacer=========='.green);
+            console.log(tarea.descripcion);
+            console.log('Estado:', tarea.completado);
+            console.log('==========================='.green);
+        }
         break;
     case 'actualizar':
-        console.log('Actualiza una tarea por hacer');
+        let actualizar = porHacer.actualizar(argv.descripcion, argv.completado);
+        console.log(actualizar);
         break;
-
+    case 'borrar':
+        let borrado = porHacer.borrar(argv.descripcion);
+        console.log(borrado);
+        break;
     default:
         console.log('Comando no reconocido');
 }
--- Cursos web/NodeJs/04- por hacer/config/yargs.js
+++ Cursos web/NodeJs/04- por hacer/config/yargs.js
@@ -1,9 +1,21 @@
-/*const argv ....
-comando crear 'Crear un elemento por hacer'
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+}
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente la tarea'
+}
 
-        --descripcion -d
-comando actualizar 'Actualiza el estado completando de una tarea'
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', descripcion)
+    .command('actualizar', 'Actualiza el estado completando de una tarea', { descripcion, completado })
+    .command('borrar', 'Borra una tarea', descripcion)
+    .help()
+    .argv;
 
---descripcion -d
---completado -c true
---help*/
+module.exports = {
+    argv
+};
