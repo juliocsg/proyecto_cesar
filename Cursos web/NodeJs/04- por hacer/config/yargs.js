62762b9a5a18924445e680873b482f36200504cd
OYM-DEV7 <oymdev7@oym.com.py>
2018-10-01 08:50:34 -0400

    yargs ejercicio

---
+++ Cursos web/NodeJs/04- por hacer/config/yargs.js
@@ -0,0 +1,9 @@
+/*const argv ....
+comando crear 'Crear un elemento por hacer'
+
+        --descripcion -d
+comando actualizar 'Actualiza el estado completando de una tarea'
+
+--descripcion -d
+--completado -c true
+--help*/
