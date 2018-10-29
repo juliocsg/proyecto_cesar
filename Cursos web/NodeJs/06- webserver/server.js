fd30b1c51d0d86a9e6401b2c8624ec839b70c540
OYM-DEV7 <oymdev7@oym.com.py>
2018-10-29 16:57:44 -0300

    webserver handlebar

--- Cursos web/NodeJs/06- webserver/server.js
+++ Cursos web/NodeJs/06- webserver/server.js
@@ -1,23 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
 
 app.use(express.static(__dirname + '/public'));
+//Express HBS engine
+//directorio para almacenar
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
 
-
-/*app.get('/', (req, res) => {
-    //res.send('Hola Mundo');
-    let salida = {
-        nombre: 'fernando',
-        edad: 32,
-        url: req.url
-    }
-    res.send(salida);
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Julio',
+        anio: new Date().getFullYear()
+    });
+})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        telefono: '0961383306',
+        anio: new Date().getFullYear()
+    })
 })
 
-app.get('/data', (req, res) => {
+/*app.get('/data', (req, res) => {
     res.send('Hola Data');
-})
-*/
+})*/
+
 app.listen(3000, () => {
     console.log("Escuchando peticiones en el puerto 3000");
 });
