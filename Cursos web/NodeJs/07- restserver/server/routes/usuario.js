a8cd140dbaff31c0bb53ecbaae795a019f0a0968
OYM-DEV7 <oymdev7@oym.com.py>
2018-11-05 16:56:34 -0300

    ejercicios con operaciones GET POST PUT y DELETE

--- Cursos web/NodeJs/07- restserver/server/routes/usuario.js
+++ Cursos web/NodeJs/07- restserver/server/routes/usuario.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const bcrypt = require('bcrypt');
 
+const _ = require('underscore');
+
 const Usuario = require('../models/usuario');
 
 const app = express();
@@ -9,9 +11,39 @@ const app = express();
 
 
 app.get('/usuario', function(req, res) {
-    res.json('get Usuario LOCAL!!!');
+    //res.json('get Usuario LOCAL!!!');
+    //{estado: true}
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+    let cambiarEstado = {
+        estado: true
+    }
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+    Usuario.find(cambiarEstado, 'nombre email role estado google img')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            Usuario.count(cambiarEstado, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cuantos: conteo
+                });
+            });
+
+        })
+
 });
 app.post('/usuario', function(req, res) {
+
     let body = req.body;
     let usuario = new Usuario({
         nombre: body.nombre,
@@ -27,27 +59,60 @@ app.post('/usuario', function(req, res) {
                 err
             });
         }
-
         //usuarioDB.password = null;
-
         res.json({
             ok: true,
             usuario: usuarioDB
         })
-
-
     });
 });
+//Actualiación
 app.put('/usuario/:id', function(req, res) {
 
     let id = req.params.id;
+    let body = _.pick(req.body, ["nombre", "email", "img", "role", "estado"]);
 
-    res.json({
-        id
+    Usuario.findByIdAndUpdate(id, body, { new: true /*, runValidators: true */ }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
     });
 });
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
+app.delete('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+
+    //Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+    let cambiaEstado = {
+        estado: false
+    }
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        };
+        if (usuarioBorrado === null) {
+            return res.status(400).json({
+                ok: false,
+                error: {
+                    message: "Usuario no encontrado"
+                }
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        })
+    });
+
 });
 
 module.exports = app;
--- Cursos web/NodeJs/07- restserver/server/server.js
+++ Cursos web/NodeJs/07- restserver/server/server.js
@@ -12,9 +12,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json());
 app.use(require('./routes/usuario'));
-
-
-
 mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
     if (err) throw err;
     console.log('Base de datos ONLINE');
