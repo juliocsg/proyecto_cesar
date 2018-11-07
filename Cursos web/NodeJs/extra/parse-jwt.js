8c6fb244db1cdb592981227edff4ab2e34325c32
OYM-DEV7 <oymdev7@oym.com.py>
2018-11-07 14:21:17 -0300

    heroku config

---
+++ Cursos web/NodeJs/extra/parse-jwt.js
@@ -0,0 +1,7 @@
+let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
+function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(window.atob(base64));
+};
