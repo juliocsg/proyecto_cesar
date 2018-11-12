d9ada4584a778b4d783b64978ef0d784043668f1
OYM-DEV7 <oymdev7@oym.com.py>
2018-11-12 14:12:10 -0300

    jwt y middleware

---
+++ Cursos web/NodeJs/07- restserver/server/middleware/autenticacion.js
@@ -0,0 +1,47 @@
+const jwt = require('jsonwebtoken');
+require('../config/config');
+/* ===========================
+ * Verificar Token
+   ==========================*/
+//otra forma = let module.exports.verificarToken ... 
+let verificarToken = (req, res, next) => {
+    //Autorización
+    let token = req.get('token');
+
+    /*res.json({
+        token: token
+    });*/
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return
+            res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'token no válido'
+                }
+            });
+        }
+        req.usuario = decoded.usuario;
+        next();
+    });
+};
+/* ===========================
+ * Verificar AdminRole
+   ==========================*/
+let verificarAdmin_Role = (req, res, next) => {
+    let usuario = req.usuario;
+    if (usuario.role === 'ADMIN_ROLE') {
+        next();
+    } else {
+        return res.json({
+            ok: false,
+            err: {
+                message: 'El usuario no es administrador'
+            }
+        });
+    }
+};
+module.exports = {
+    verificarToken,
+    verificarAdmin_Role
+};
--- Cursos web/NodeJs/07- restserver/server/routes/usuario.js
+++ Cursos web/NodeJs/07- restserver/server/routes/usuario.js
@@ -6,11 +6,20 @@ const _ = require('underscore');
 
 const Usuario = require('../models/usuario');
 
+const { verificarToken, verificarAdmin_Role } = require('../middleware/autenticacion');
+
 const app = express();
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificarToken, (req, res) => {
     //res.json('get Usuario LOCAL!!!');
     //{estado: true}
+    return res.json({
+        usuario: req.usuario,
+        nombre: req.usuario.nombre,
+        email: req.usuario.email
+
+    });
+
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -36,10 +45,9 @@ app.get('/usuario', function(req, res) {
                     cuantos: conteo
                 });
             });
-        })
+        });
 });
-app.post('/usuario', function(req, res) {
-
+app.post('/usuario', [verificarToken, verificarAdmin_Role], (req, res) => {
     let body = req.body;
     let usuario = new Usuario({
         nombre: body.nombre,
@@ -63,8 +71,7 @@ app.post('/usuario', function(req, res) {
     });
 });
 //Actualiación
-app.put('/usuario/:id', function(req, res) {
-
+app.put('/usuario/:id', [verificarToken, verificarAdmin_Role], function(req, res) {
     let id = req.params.id;
     let body = _.pick(req.body, ["nombre", "email", "img", "role", "estado"]);
 
@@ -81,7 +88,7 @@ app.put('/usuario/:id', function(req, res) {
         });
     });
 });
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificarToken, verificarAdmin_Role], function(req, res) {
     let id = req.params.id;
 
     //Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
