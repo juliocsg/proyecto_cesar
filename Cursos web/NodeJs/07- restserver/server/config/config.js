ef997788a03aa0cc617d0e9790601d745c731d10
OYM-DEV7 <oymdev7@oym.com.py>
2018-11-14 16:43:19 -0300

    Google con postman

--- Cursos web/NodeJs/07- restserver/server/config/config.js
+++ Cursos web/NodeJs/07- restserver/server/config/config.js
@@ -39,4 +39,4 @@ process.env.URLDB = urlDB;
 // Google Client ID
 //========================
 
-process.env.CLIENT_ID = process.env.CLIENT_ID || 529324214219 - 'rhgor0c1ktt39l8rs7q9uclg5p21svvo.apps.googleusercontent.com';
+process.env.CLIENT_ID = process.env.CLIENT_ID || '529324214219-rhgor0c1ktt39l8rs7q9uclg5p21svvo.apps.googleusercontent.com';
