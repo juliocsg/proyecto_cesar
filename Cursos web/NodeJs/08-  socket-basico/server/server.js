b5b2314955e92d07a7b8ca9971982ab9354d0def
OYM-DEV7 <oymdev7@oym.com.py>
2018-11-30 09:43:15 -0300

    socket: chat uno a uno y en grupo

--- Cursos web/NodeJs/08-  socket-basico/server/server.js
+++ Cursos web/NodeJs/08-  socket-basico/server/server.js
@@ -1,8 +1,12 @@
+const http = require('http');
+
+const socketIO = require('socket.io');
 const express = require('express');
 
 const path = require('path');
 
 const app = express();
+const server = http.createServer(app);
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -10,8 +14,11 @@ const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
 
+// IO = esta es la comunicacion del backend
+module.exports.io = socketIO(server);
+require('./sockets/socket');
 
-app.listen(port, (err) => {
+server.listen(port, (err) => {
 
     if (err) throw new Error(err);
 
