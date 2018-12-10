dace515aa6f738a744a86a81f84feab957cb3d92
Julio César <jcesarsaucedo1993@gmail.com>
2018-12-09 23:13:03 -0300

    ticket-control

--- Cursos web/NodeJs/09- socket-colas/server/classes/ticket-control.js
+++ Cursos web/NodeJs/09- socket-colas/server/classes/ticket-control.js
@@ -1,25 +1,43 @@
-const fs = require('fs');
-
+const fs = require('fs-extra');
+//cp.copy.writeFileSync();
 class TicketControl {
     constructor() {
         this.ultimo = 0;
         this.hoy = new Date().getDate();
-        let data = require('./../datos/data.json');
+        let data = require('../datos/datos.json');
 
         if (data.hoy === this.hoy) {
-
+            this.ultimo = data.ultimo;
         } else {
             this.reiniciarConteo();
         }
     }
+    siguiente(){
+      this.ultimo+=1;
+      this.grabarArchivo();
+      return `Ticket ${this.ultimo}`;
+    }
     reiniciarConteo() {
-        let jsonData = {
-            ultimo: this.ultimo,
-            hoy: this.hoy
-        };
-        let jsonDataString = JSON.stringify(jsonData);
-        fs.writeFileSync('./../datos/data.json', jsonDataString);
-        console.log('Se ha inicializado el sistema');
+      this.ultimo = 0;
+      console.log('Se ha inicializado el sistema');
+      this.grabarArchivo();
+    }
+    grabarArchivo(){
+         //inciciales
+         let jsonDataInicial = { "ultimo": 10, "hoy": 0 };
+         let jsonData = {
+             ultimo: this.ultimo,
+             hoy: this.hoy
+         };
+         //const jsonDataString = JSON.stringify(jsonData);
+         //let textoNormal = "hola mundo";
+         fs.writeJSONSync('./server/datos/datos.json', jsonData, (err) => {
+             if (err) {
+                 console.error(err);
+             }
+         });
+         //fs.writeFileSync('/server/datos/data.json', jsonDataString);
+         console.log('Datos guardados');
     }
 }
 module.exports = {
