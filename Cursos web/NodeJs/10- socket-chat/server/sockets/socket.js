572b41deb402ca3b1f09900741fa2a6afb482a55
Julio Saucedo <jcesarsaucedo1993@gmail.com>
2018-12-13 19:05:55 -0300

    socket chat en salas y privados

--- Cursos web/NodeJs/10- socket-chat/server/sockets/socket.js
+++ Cursos web/NodeJs/10- socket-chat/server/sockets/socket.js
@@ -1,42 +1,36 @@
 const { io } = require('../server');
-
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utilidades/utilidades');
+const usuarios = new Usuarios();
 
 io.on('connection', (client) => {
+    client.on('entrarChat',(data, callback)=>{
+        if(!data.nombre || !data.sala){
+            return callback({
+                error:true,
+                mensaje:'El nombre/sala es necesario'
+            });
+        }
+        client.join(data.sala);
 
-    console.log('Usuario conectado');
-
-    client.emit('enviarMensaje', {
-        usuario: 'Administrador',
-        mensaje: 'Bienvenido a esta aplicación'
+        usuarios.agregarPersona( client.id, data.nombre , data.sala);
+        client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
+        callback(usuarios.getPersonasPorSala(data.sala));
     });
-
-
-
-    client.on('disconnect', () => {
-        console.log('Usuario desconectado');
+    client.on('crearMensaje',(data) =>{
+        let persona = usuarios.getPersona(client.id);
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
     });
-
-    // Escuchar el cliente
-    client.on('enviarMensaje', (data, callback) => {
-
-        console.log(data);
-
-        client.broadcast.emit('enviarMensaje', data);
-
-
-        // if (mensaje.usuario) {
-        //     callback({
-        //         resp: 'TODO SALIO BIEN!'
-        //     });
-
-        // } else {
-        //     callback({
-        //         resp: 'TODO SALIO MAL!!!!!!!!'
-        //     });
-        // }
-
-
-
+    client.on('disconnect', ()=>{
+        let personaBorrada =  usuarios.borrarPersona(client.id);
+        //console.log(personaBorrada.);
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${personaBorrada.nombre} salió`));
+        client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala));
     });
-
+    //Mensajes privados
+    client.on('mensajePrivado', (data) =>{
+        let persona = usuarios.getPersona(client.id);
+        client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mensaje));
+    });
 });
