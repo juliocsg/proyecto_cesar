70017ac31f65d1f05192fa6bb2f522db33999202
OYM-DEV7 <oymdev7@oym.com.py>
2018-12-14 10:43:30 -0300

    diseño de aplicación web de chat y salas

--- Cursos web/NodeJs/10- socket-chat/server/sockets/socket.js
+++ Cursos web/NodeJs/10- socket-chat/server/sockets/socket.js
@@ -4,32 +4,36 @@ const { crearMensaje } = require('../utilidades/utilidades');
 const usuarios = new Usuarios();
 
 io.on('connection', (client) => {
-    client.on('entrarChat',(data, callback)=>{
-        if(!data.nombre || !data.sala){
+    client.on('entrarChat', (data, callback) => {
+        if (!data.nombre || !data.sala) {
             return callback({
-                error:true,
-                mensaje:'El nombre/sala es necesario'
+                error: true,
+                mensaje: 'El nombre/sala es necesario'
             });
         }
         client.join(data.sala);
 
-        usuarios.agregarPersona( client.id, data.nombre , data.sala);
+        usuarios.agregarPersona(client.id, data.nombre, data.sala);
         client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
+
+        client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${data.nombre} se unió`));
+
         callback(usuarios.getPersonasPorSala(data.sala));
     });
-    client.on('crearMensaje',(data) =>{
+    client.on('crearMensaje', (data, callback) => {
         let persona = usuarios.getPersona(client.id);
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
         client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+        callback(mensaje);
     });
-    client.on('disconnect', ()=>{
-        let personaBorrada =  usuarios.borrarPersona(client.id);
+    client.on('disconnect', () => {
+        let personaBorrada = usuarios.borrarPersona(client.id);
         //console.log(personaBorrada.);
         client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${personaBorrada.nombre} salió`));
         client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala));
     });
     //Mensajes privados
-    client.on('mensajePrivado', (data) =>{
+    client.on('mensajePrivado', (data) => {
         let persona = usuarios.getPersona(client.id);
         client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mensaje));
     });
